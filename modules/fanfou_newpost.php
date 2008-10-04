9e91f54eabd607adac6d37ffc3ddb2e0c4ec24a3
VerdanaMu <VerdanaMu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-04 00:08:42 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fanfou-tools/trunk@67355 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/fanfou_newpost.php
+++ modules/fanfou_newpost.php
@@ -1,3 +1,12 @@
 <?php
-echo "incoming sooon";
 /* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+?>
+<div class="wrap">
+<h2>Fanfou Tools v<?php echo FANFOU_TOOLS_VER;?> - <em style="color: gray; font-size: 18px;"><?php echo  _f('Update Fanfou Status'); ?></em></h2>
+<?php include_once('tab_navbar.php');?>
+
+Incoming soon...
+</div>
+
+
