eb762cb6fb3852040a9d013e672b65d77a280481
VerdanaMu <VerdanaMu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-04 19:02:40 +0000

    New module: followers
    git-svn-id: https://plugins.svn.wordpress.org/fanfou-tools/trunk@67412 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ modules/fanfou_followers.php
@@ -0,0 +1,7 @@
+<div class="wrap">
+    <h2>Fanfou Tools v<?php echo FANFOU_TOOLS_VER;?> - <em style="color: gray; font-size: 18px;"><?php echo  _f('Your fanfou followers'); ?></em></h2>
+    <?php include_once('tab_navbar.php');?>
+
+    <p><b>NOT IMPLEMENTED</b></p>
+</div>
+
