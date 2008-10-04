1afdfdf70a37135b917110ff4932f3b8e7cf120d
VerdanaMu <VerdanaMu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-04 22:46:40 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fanfou-tools/trunk@67440 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/fanfou_newpost.php
+++ modules/fanfou_newpost.php
@@ -1,11 +1,43 @@
-<?php
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
+        <div class="wrap">
+            '.fanfou_post_form().'
+        </div>
 
-?>
+
 <div class="wrap">
-    <h2>Fanfou Tools v<?php echo FANFOU_TOOLS_VER;?> - <em style="color: gray; font-size: 18px;"><?php echo _f('Write New'); ?></em></h2>
-    <?php include_once('tab_navbar.php');?>
 
-    <p><b>NOT IMPLEMENTED</b></p>
+<h2>Fanfou Tools v<?php echo FANFOU_TOOLS_VER;?> - <em style="color: gray; font-size: 18px;"><?php echo _f('Write New'); ?></em></h2>
+<?php include_once('tab_navbar.php');?>
+<p>
+<?php echo _f('This will create a new \'Fanfou\' status in <a href="http://fanfou.com">Fanfou</a> using the account information in your <a href="./admin.php?page=fanfou-tools.php">Fanfou Tools Options</a>.');?><br/>
+<?php echo _f('You can use the UBBCode <span style="color: red">[tiny][/tiny]</span> to automatically convert an URL into a Tiny URL.');?>
+</p>
+
+<form action="'.get_bloginfo('wpurl').'/wp-admin/post-new.php?page=fanfou-tools.php" method="post" id="fanfou_post_form">
+<fieldset>
+<p><textarea type="text" cols="60" rows="5" maxlength="140" id="fanfou_status_text" name="fanfou_status_text" onkeyup="fanfouCharCount();"></textarea></p>
+<input type="hidden" name="fanfou_action" value="post_status" />
+<script type="text/javascript">
+//<![CDATA[
+function fanfouCharCount()
+{
+    var count = document.getElementById("fanfou_status_text").value.length;
+    if (count > 0) {
+        document.getElementById("fanfou_char_count").innerHTML = (140 - count) + "<?php echo _f(' characters remaining');?>";
+    }
+    else {
+        document.getElementById("fanfou_char_count").innerHTML = "";
+    }
+}
+setTimeout("fanfouCharCount();", 500);
+document.getElementById("fanfou_post_form").setAttribute("autocomplete", "off");
+//]]>
+</script>
+<p>
+<input type="submit" id="fanfou_submit" name="fanfou_submit" value="<?php echo _f('Post Fanfou Status!');?>" />
+<span id="fanfou_char_count"></span>
+</p>
+<div class="clear"></div>
+</fieldset>
+</form>
 </div>
 
